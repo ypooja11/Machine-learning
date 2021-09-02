234759cd13cf6535b0d0ff66b10cf8ea2d2fdde2
ypooja11 <poojayadav0811@gmail.com>
2021-09-02 14:59:45 -0400

    Final version

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,47 +1,73 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 data_file_name=input("Enter the name of your data file:")
-file_Object=open('C:/Users/pooja/.spyder-py3/'+data_file_name,"r")
+file_Object=open(data_file_name,"r")
 
 first_line=file_Object.readline()
 first_line_array=first_line.split()
-max_rows=int(first_line_array[0])
-no_of_elements=int(first_line_array[1])
+row=int(first_line_array[0])
 
-array1=np.zeros[max_rows,(no_of_elements+1)]
-for i in range (max_rows):
+def axis_label(a):
+    if a==0:
+        return("Sepal length")
+    elif a==1:
+        return("Sepal width")
+    elif a==2:
+        return("Petal length")
+    elif a==3:
+        return("Petal width")
+    
+coordinates=np.zeros([row,5])
+for i in range (row):
     coordinates_string=file_Object.readline()
     data=coordinates_string.split("\t")
-    for j in range(no_of_elements):
-        array1[i,j]=float(data[j])
-    array1[i,no_of_elements]=data[no_of_elements]
-
-
+    for j in range(4):
+        coordinates[i,j]=float(data[j])
+    if data[4]=="Setosa\n":
+      coordinates[i,4]=0
+    elif data[4]=="Versicolor\n":
+      coordinates[i,4]=1
+    else:
+      coordinates[i,4]=2
     
-#print(feature_array)
+while True:
+  print('''You can do a plot of any two features of the Iris Data set
+  The feature codes are:
+  0 = sepal length
+  1 = sepal width
+  2 = petal length
+  3 = petal width''')
 
-print('''You can do a plot of any two features of the Iris Data set
-The feature codes are:
-0 = sepal length
-1 = sepal width
-2 = petal length
-3 = petal width''')
+  horizontal_axis=int(input("Enter feature code for the horizontal axis: "))
+  vertical_axis=int(input("Enter feature code for the vertical axis: "))
+  flag1,flag2,flag3=1,1,1
+  for k in range(row):
+      if coordinates[k,4]==0:
+        if flag1==1:
+          plt.plot(coordinates[k,horizontal_axis],coordinates[k,vertical_axis],color='orange',marker='o',label='Setosa')
+          flag1=0
+        plt.plot(coordinates[k,horizontal_axis],coordinates[k,vertical_axis],color='orange',marker='o')
+      elif coordinates[k,4]==1:
+          if flag2==1:
+            plt.plot(coordinates[k,horizontal_axis],coordinates[k,vertical_axis],c='blue',marker='x',label='Versicolor')
+            flag2=0
+          plt.plot(coordinates[k,horizontal_axis],coordinates[k,vertical_axis],c='blue',marker='x')
+      elif coordinates[k,4]==2:
+          if flag3==1:
+            plt.plot(coordinates[k,horizontal_axis],coordinates[k,vertical_axis],c='yellow',marker='s',label='Virginica')
+            flag3=0
+          plt.plot(coordinates[k,horizontal_axis],coordinates[k,vertical_axis],c='yellow',marker='s')
 
-horizontal_axis=int(input("Enter feature code for the horizontal axis: "))
-vertical_axis=int(input("Enter feature code for the vertical axis: "))
+  plt.legend()
+  plt.xlabel(axis_label(horizontal_axis))
+  plt.ylabel(axis_label(vertical_axis))
+  plt.title('Iris flower plot')
 
-for k in range (max_rows):
-    if array1[k,no_of_elements]=='Setosa':
-        plt.scatter(array1[horizontal_axis,vertical_axis],c='orange',marker='o')
-    elif array1[k,no_of_elements]=='Versicolor':
-        plt.scatter(array1[horizontal_axis,vertical_axis],c='blue',marker='x')
-    elif array1[k,no_of_elements]=='Virginica':
-        plt.scatter(array1[horizontal_axis,vertical_axis],c='yellow',marker='s')
-#plt.ylabel('Y')
-plt.show()
+  plt.show()
+  option=(input("Do you want another plot(Y/N):"))
+  if option=="N" or option=='n':
+    break
 
-#plt.plot([2,4],[6,8],'bo')
-#plt.ylabel('Y')
-#plt.show()
+
